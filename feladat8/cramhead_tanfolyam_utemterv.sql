8dca38bf643119497e69a0aa46e436301f4b622b
Creever <istvan.kispal@clapp.eu>
2018-12-13 17:51:31 +0100

    sda

--- feladat8/cramhead_tanfolyam_utemterv.sql
+++ feladat8/cramhead_tanfolyam_utemterv.sql
@@ -1,14 +1,60 @@
-select kezdodatum, vegedatum, kod, nev, felelos, hely, oktato, maxletszam,
-sum(ideig) ideiglenes,  sum(bizt) biztos, sum(ideig) + sum(bizt) osszes, sum(kesz) keszen
-from
-(select t.kezdodatum, t.vegedatum, t.kod, tt.nev, tt.oktato_kod felelos, h.nev hely, t.oktato_kod oktato, tt.maxletszam,
-(case when statusz = 'Ideiglenes' then 1 else 0 end) ideig, 
-(case when statusz = 'Megerositett' then 1 else 0 end) bizt, 
-(case when statusz = 'Ideiglenes + Készenlét' then 1 else 0 end) kesz
-from tanfolyam t, tanfolyam_tipus tt, helyszin h,
-foglalas f
-where tt.kod = t.tanfolyam_tipus_kod
-and t.helyszin_kod = h.kod
-and t.kod = f.tanfolyam_kod (+))
-group by kezdodatum, vegedatum, kod, nev,felelos, hely, oktato, maxletszam
-order by kezdodatum, kod
+create or replace PROCEDURE TANFOLYAM_UTEMTERV IS
+    p_code VARCHAR2(255);
+    p_name VARCHAR2(255);
+    p_address VARCHAR2(255);
+    megerositett INTEGER;
+    ideiglenes INTEGER;
+    keszenlet VARCHAR2(255);
+    osszesen INTEGER;
+    
+     ct_code VARCHAR2(255);
+     ct_name VARCHAR2(255);
+     ct_min VARCHAR2(255);
+     ct_max VARCHAR2(255);
+BEGIN
+
+    dbms_output.put_line(
+        'Dátum' || chr(9) || 
+        'Tanfkód' || chr(9) || 
+        'Tanf leírás' || chr(9) ||
+        'Tanf felelõs' || chr(9) ||
+        'Tanf helye' || chr(9) ||
+        'Elõadó' || chr(9) ||
+        'Max' || chr(9) ||
+        'ideig' || chr(9) ||
+        'biztos' || chr(9) ||
+        'Össz' || chr(9) ||
+        'Készn' || chr(9) ||
+        'Mégj'
+    );
+
+    FOR item IN 
+        (
+            SELECT * FROM course ORDER BY start_date
+        )
+    LOOP
+
+        SELECT code, name, address into p_code, p_name, p_address FROM place WHERE code = item.place_code;
+        SELECT code, name, min, max into ct_code, ct_name, ct_min, ct_max FROM course_type WHERE code = item.course_type_code;
+        SELECT COUNT(*) into megerositett FROM booking WHERE course_code = item.code AND status='Megerositett';
+        SELECT COUNT(*) into ideiglenes FROM booking WHERE course_code = item.code AND status='Ideiglenes';
+        SELECT COUNT(*) into ideiglenes FROM booking WHERE course_code = item.code AND status='Ideiglenes + Készenlét';
+        
+        osszesen := megerositett + ideiglenes;
+        
+        dbms_output.put_line(
+            to_char(item.start_date, 'yyyy-mm-dd') || ' - ' || to_char(item.end_date, 'yyyy-mm-dd') || chr(9) || 
+            item.code || chr(9) || 
+            item.description || chr(9) ||
+            item.teacher_code || chr(9) ||
+            p_address || chr(9) ||
+            p_name || chr(9) ||
+            ct_max || chr(9) ||
+            ideiglenes || chr(9) ||
+            megerositett || chr(9) ||
+            osszesen || chr(9) ||
+            keszenlet || chr(9) ||
+            item.note
+        );
+    END LOOP;
+END;
