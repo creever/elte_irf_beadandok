daf1fd69f3251381c3a8bb8c1be5cc7a53d16318
Creever <istvan.kispal@clapp.eu>
2018-12-13 16:58:21 +0100

    cramhead export dump

---
+++ feladat8/cramhead_export_dump.sql
@@ -0,0 +1,103 @@
+--------------------------------------------------------
+--  File created - csütörtök-december-13-2018   
+--------------------------------------------------------
+REM INSERTING into AWUD3U.BOOKING
+SET DEFINE OFF;
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Jakab','CSTJAN1','client1',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Andrea','CSTJAN1','client1',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Elek','CSTJAN1','client1',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Tibor','CSTJAN1','client1',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Gergely','CSTJAN1','client1',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Jakab','CSTJAN1','client2',to_date('17-DEC-21','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Andrea','CSTJAN1','client2',to_date('17-DEC-21','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Elek','CSTJAN1','client2',to_date('17-DEC-21','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Tibor','CSTJAN1','client2',to_date('17-DEC-21','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Gergely','CSTJAN1','client2',to_date('17-DEC-21','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Jakab','CSTJAN1','client3',to_date('17-DEC-22','RR-MON-DD'),'Ideiglenes + Készenlét');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Andrea','CSTJAN1','client3',to_date('17-DEC-22','RR-MON-DD'),'Ideiglenes + Készenlét');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Gizi','CSTJAN2','client2',to_date('17-DEC-10','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Alma','CSTJAN2','client2',to_date('17-DEC-10','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Viola','CSTJAN2','client2',to_date('17-DEC-10','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Endre','CSTJAN2','client2',to_date('17-DEC-10','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Dalma','CSTJAN2','client2',to_date('17-DEC-10','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Alma','CSTFEB1','client3',to_date('17-DEC-15','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Gergely','CSTFEB1','client3',to_date('17-DEC-15','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Tibor','CSTFEB1','client3',to_date('17-DEC-15','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Elek','CSTFEB1','client3',to_date('17-DEC-15','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Endre','CSTFEB1','client3',to_date('17-DEC-15','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Alma','EXSJAN1','client1',to_date('17-DEC-18','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Endre','EXSJAN1','client1',to_date('17-DEC-18','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Viola','EXSJAN1','client1',to_date('17-DEC-18','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Attila','EXSJAN1','client1',to_date('17-DEC-18','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Imre','EXSJAN1','client1',to_date('17-DEC-18','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Ambrus','EXSJAN1','client1',to_date('17-DEC-18','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Gizi','EXSFEB1','client2',to_date('17-DEC-28','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Endre','EXSFEB1','client2',to_date('17-DEC-28','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Dalma','EXSFEB1','client2',to_date('17-DEC-28','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Jakab','EXSFEB1','client2',to_date('17-DEC-28','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Ambrus','EXSFEB1','client2',to_date('17-DEC-28','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Gergely','STSJAN1','client3',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Elek','STSJAN1','client3',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Gizi','STSJAN1','client3',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Tibor','STSJAN1','client3',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Lajos','STSJAN1','client3',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Nagy Imre','STSJAN1','client3',to_date('17-DEC-20','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Andrea','STSFEB1','client1',to_date('17-DEC-22','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Imre','STSFEB1','client1',to_date('17-DEC-22','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Gergely','STSFEB1','client1',to_date('17-DEC-22','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Gergely','STSFEB1','client2',to_date('17-DEC-21','RR-MON-DD'),'Ideiglenes');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Elek','STSFEB1','client2',to_date('17-DEC-21','RR-MON-DD'),'Ideiglenes');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Andrea','STSFEB1','client2',to_date('17-DEC-21','RR-MON-DD'),'Ideiglenes');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Andrea','KZSFEB1','client2',to_date('17-DEC-12','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Elek','KZSFEB1','client2',to_date('17-DEC-12','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Kiss Gergely','KZSFEB1','client2',to_date('17-DEC-12','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Andrea','KZSFEB1','client1',to_date('17-DEC-23','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Endre','KZSFEB1','client1',to_date('17-DEC-23','RR-MON-DD'),'Megerositett');
+Insert into AWUD3U.BOOKING (PARTICIPANT,COURSE_CODE,CLIENT_NAME,"date",STATUS) values ('Lakatos Elek','KZSFEB1','client1',to_date('17-DEC-23','RR-MON-DD'),'Megerositett');
+REM INSERTING into AWUD3U.CLIENT
+SET DEFINE OFF;
+Insert into AWUD3U.CLIENT (NAME,ADDRESS,CONTACT,PHONE) values ('client1','Cim1','Kapcs1','6666666');
+Insert into AWUD3U.CLIENT (NAME,ADDRESS,CONTACT,PHONE) values ('client2','Cim2','Kapcs2','7777777');
+Insert into AWUD3U.CLIENT (NAME,ADDRESS,CONTACT,PHONE) values ('client3','Cim3','Kapcs3','8888888');
+REM INSERTING into AWUD3U.COURSE
+SET DEFINE OFF;
+Insert into AWUD3U.COURSE (CODE,START_DATE,END_DATE,TEACHER_CODE,COURSE_TYPE_CODE,PLACE_CODE) values ('CSTJAN1',to_date('18-JAN-15','RR-MON-DD'),to_date('18-JAN-19','RR-MON-DD'),'GJ','CST','PH');
+Insert into AWUD3U.COURSE (CODE,START_DATE,END_DATE,TEACHER_CODE,COURSE_TYPE_CODE,PLACE_CODE) values ('CSTJAN2',to_date('18-JAN-20','RR-MON-DD'),to_date('18-JAN-24','RR-MON-DD'),'NB','CST','PH');
+Insert into AWUD3U.COURSE (CODE,START_DATE,END_DATE,TEACHER_CODE,COURSE_TYPE_CODE,PLACE_CODE) values ('CSTFEB1',to_date('18-FEB-15','RR-MON-DD'),to_date('18-FEB-19','RR-MON-DD'),'KJ','CST','PH');
+Insert into AWUD3U.COURSE (CODE,START_DATE,END_DATE,TEACHER_CODE,COURSE_TYPE_CODE,PLACE_CODE) values ('EXSJAN1',to_date('18-JAN-15','RR-MON-DD'),to_date('18-JAN-19','RR-MON-DD'),'VD','EXS','ZH');
+Insert into AWUD3U.COURSE (CODE,START_DATE,END_DATE,TEACHER_CODE,COURSE_TYPE_CODE,PLACE_CODE) values ('EXSFEB1',to_date('18-FEB-15','RR-MON-DD'),to_date('18-FEB-19','RR-MON-DD'),'NB','EXS','ZH');
+Insert into AWUD3U.COURSE (CODE,START_DATE,END_DATE,TEACHER_CODE,COURSE_TYPE_CODE,PLACE_CODE) values ('STSJAN1',to_date('18-JAN-20','RR-MON-DD'),to_date('18-JAN-24','RR-MON-DD'),'GJ','STS','FH');
+Insert into AWUD3U.COURSE (CODE,START_DATE,END_DATE,TEACHER_CODE,COURSE_TYPE_CODE,PLACE_CODE) values ('STSFEB1',to_date('18-FEB-20','RR-MON-DD'),to_date('18-FEB-24','RR-MON-DD'),'NB','STS','FH');
+Insert into AWUD3U.COURSE (CODE,START_DATE,END_DATE,TEACHER_CODE,COURSE_TYPE_CODE,PLACE_CODE) values ('KZSFEB1',to_date('18-FEB-10','RR-MON-DD'),to_date('18-FEB-14','RR-MON-DD'),'VD','KZS','ZH');
+REM INSERTING into AWUD3U.COURSE_PRICE
+SET DEFINE OFF;
+Insert into AWUD3U.COURSE_PRICE (COURSE_TYPE_CODE,VALID,PRICE) values ('CST',to_date('18-JAN-01','RR-MON-DD'),'30000');
+Insert into AWUD3U.COURSE_PRICE (COURSE_TYPE_CODE,VALID,PRICE) values ('EXS',to_date('18-JAN-01','RR-MON-DD'),'40000');
+Insert into AWUD3U.COURSE_PRICE (COURSE_TYPE_CODE,VALID,PRICE) values ('STS',to_date('18-JAN-01','RR-MON-DD'),'25000');
+Insert into AWUD3U.COURSE_PRICE (COURSE_TYPE_CODE,VALID,PRICE) values ('KZS',to_date('18-JAN-01','RR-MON-DD'),'50000');
+Insert into AWUD3U.COURSE_PRICE (COURSE_TYPE_CODE,VALID,PRICE) values ('CST',to_date('18-FEB-01','RR-MON-DD'),'35000');
+Insert into AWUD3U.COURSE_PRICE (COURSE_TYPE_CODE,VALID,PRICE) values ('EXS',to_date('18-FEB-01','RR-MON-DD'),'45000');
+Insert into AWUD3U.COURSE_PRICE (COURSE_TYPE_CODE,VALID,PRICE) values ('STS',to_date('18-FEB-01','RR-MON-DD'),'30000');
+REM INSERTING into AWUD3U.COURSE_TYPE
+SET DEFINE OFF;
+Insert into AWUD3U.COURSE_TYPE (CODE,NAME,MIN,MAX,TEACHER_CODE) values ('CST','Cégek stratégiai Tervezése','5','10','GJ');
+Insert into AWUD3U.COURSE_TYPE (CODE,NAME,MIN,MAX,TEACHER_CODE) values ('EXS','Exportáljunk a sikerért','5','10','VD');
+Insert into AWUD3U.COURSE_TYPE (CODE,NAME,MIN,MAX,TEACHER_CODE) values ('STS','Stratégiai tervezés','5','10','GJ');
+Insert into AWUD3U.COURSE_TYPE (CODE,NAME,MIN,MAX,TEACHER_CODE) values ('KZS','Közszereplés','5','10','VD');
+REM INSERTING into AWUD3U.GRADE
+SET DEFINE OFF;
+Insert into AWUD3U.GRADE (COURSE_CODE,AMOUNT,AVERAGE,TOPIC) values ('CSTJAN1','8','4,25','Cegismeretek');
+Insert into AWUD3U.GRADE (COURSE_CODE,AMOUNT,AVERAGE,TOPIC) values ('CSTJAN1','6','4,5','Cegjog');
+Insert into AWUD3U.GRADE (COURSE_CODE,AMOUNT,AVERAGE,TOPIC) values ('CSTJAN2','5','4,8','Cegismeretek');
+Insert into AWUD3U.GRADE (COURSE_CODE,AMOUNT,AVERAGE,TOPIC) values ('CSTJAN2','5','4,2','Cegjog');
+REM INSERTING into AWUD3U.PLACE
+SET DEFINE OFF;
+Insert into AWUD3U.PLACE (CODE,NAME,PHONE,ADDRESS) values ('PH','Piros ház','Piros u. 1.','1111111');
+Insert into AWUD3U.PLACE (CODE,NAME,PHONE,ADDRESS) values ('FH','Fehér ház','Washington u. 1.','2222222');
+Insert into AWUD3U.PLACE (CODE,NAME,PHONE,ADDRESS) values ('ZH','Zöld ház','Zöld u. 1.','3333333');
+REM INSERTING into AWUD3U.TEACHER
+SET DEFINE OFF;
+Insert into AWUD3U.TEACHER (CODE,NAME,COURSE_MANAGER) values ('GJ','Gipsz Jakab','1');
+Insert into AWUD3U.TEACHER (CODE,NAME,COURSE_MANAGER) values ('NB','Nagy Béla','0');
+Insert into AWUD3U.TEACHER (CODE,NAME,COURSE_MANAGER) values ('KJ','Kiss János','0');
+Insert into AWUD3U.TEACHER (CODE,NAME,COURSE_MANAGER) values ('VD','Vad Dalma','1');
